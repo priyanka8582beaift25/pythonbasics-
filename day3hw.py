991ff4df74c89223542b25f70a83714f082f2afd
priyanka8582beaift25 <priyanka8582.beaift25@chitkara.edu.in>
2025-08-25 23:58:48 +0530

    Add files via upload

---
+++ day3hw.py
@@ -0,0 +1,23 @@
+#print the first 10 natural number 
+for i in range (1,11):
+    print(i)
+    
+    
+    
+#print the multiplication table of a number 
+number=5
+for i in range (1,11) :
+    result = number * i 
+    print("5*",i,"=",result)
+    
+    
+    
+#print the square of numbers from 1 to 10
+for i in range (1,11) :
+    print("Square of",i,"is",i**2)
+    
+    
+
+#print all even numbers from 1 to 100
+for i in range (0,100,2) :
+    print("Even numbers are:",i+2)
