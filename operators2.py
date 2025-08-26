151c466e7a6a9b2dafb1e40b8e3035c61d3a2695
priyanka8582beaift25 <priyanka8582.beaift25@chitkara.edu.in>
2025-08-26 23:18:36 +0530

    Add files via upload

---
+++ operators2.py
@@ -0,0 +1,11 @@
+# Program to find the sum, difference, product, and division of two numbers 
+num1 = int(input("enter num1:"))
+num2 = int(input("enter num2:"))
+sum = num1 +num2 
+diff = num1 - num2 
+product = num1*num2 
+division = num1/num2 
+print ("The sum of numbers is :",sum)
+print("The diff of numbers is : ",diff)
+print("The product of numbers is :",product)
+print("The division of numbers is :",division)
