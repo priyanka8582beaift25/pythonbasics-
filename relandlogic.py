e83c906b83822f76ce0e6ec6bb0d60a46a16c61f
priyanka8582beaift25 <priyanka8582.beaift25@chitkara.edu.in>
2025-08-26 23:54:49 +0530

    Add files via upload

---
+++ relandlogic.py
@@ -0,0 +1,7 @@
+# Program to check whether a given number is greater than 100 and divisible by 5 
+while True :
+    a = int(input("Enter any number :"))
+    if a>100 and a %5==0 :
+        print("the statement is True ")
+    else : 
+        print("The statement is False")
