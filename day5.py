64a2111af8eda0df013e11785528b405fc3981b5
priyanka8582beaift25 <priyanka8582.beaift25@chitkara.edu.in>
2025-08-28 11:04:33 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,56 @@
+
+
+
+
+
+# rows = 5
+# for i in range (1,rows+1): 
+    # for j in range (i):
+        # print (1,end="")
+    # print()
+    
+#rows = 5
+#for i in range (1,rows+1): 
+    #for j in range (i):
+        # print (2,end="")
+    # print()
+    
+    
+    
+# rows = 5
+# for i in range (1,rows+1): 
+    # for j in range (i):
+        # print (i,end="")
+    # print()
+    
+    
+# rows = 8 
+# for i in range (1,rows+1) :
+    # # j in range (i) :
+        # print(j+i,end="")
+    # print()  
+    
+    
+
+# rows = 5
+# for i in range (rows+1,0,-1): 
+    # for j in range (i) :
+        # print ("*",end=" ")
+    # print()              
+    
+    
+#rows = 5
+# for i in range (1,rows+1): 
+    # for j in range (i,) :
+        # print ("*",end=" ")
+    # print()    
+    
+    
+
+rows = 10
+for i in range (rows): 
+    for j in range (rows-i-1) :
+        print (" ",end=" " )
+    for j in range(1*i+1) :
+        print(" ",end="*")
+    print()            
